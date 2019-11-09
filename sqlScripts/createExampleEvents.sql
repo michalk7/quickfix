74ad0469c28d96567b2faf866e22f214f06c0010
Michał Kubara <michalkubara7@gmail.com>
2019-11-09 13:23:49 +0100

    Utworzenie skryptu dodającego kilka eventów do bazy.

---
+++ sqlScripts/createExampleEvents.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO inzynierka_db.event (confirmed, end_date, start_date, event_name, problem_description, problem_title, event_expert_id, event_user_id)
+VALUES(false, '2019-11-06T15:00:00', '2019-11-06:14:00:00', 'Naprawa u mailTest', 'Pomniejsze naprawy w domu', 'Naprawy', 7, 2);
+
+INSERT INTO inzynierka_db.event (confirmed, end_date, start_date, event_name, problem_description, problem_title, event_expert_id, event_user_id)
+VALUES(false, '2019-11-05T10:00:00', '2019-11-05:09:00:00', 'Usługa u mailTest', 'Rura w kuchni się popsuła i cieknie', 'Cieknąca rura', 6, 2);
+
+INSERT INTO inzynierka_db.event (confirmed, end_date, start_date, event_name, problem_description, problem_title, event_expert_id, event_user_id)
+VALUES(false, '2019-09-05T10:00:00', '2019-09-05:09:00:00', 'Pomoc mailTest', 'Układanie płytek w łazience', 'Układanie płytek', 7, 2);
